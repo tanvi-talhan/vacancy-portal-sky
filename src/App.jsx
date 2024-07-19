429582e602f7e0204ac9c0c83635a68c51de2950
Radshu21 <tanvitalhan@gmail.com>
2024-07-19 13:18:09 +0530

    Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import CandidateList from './Pages/CandidateList'
 import Login from './Components/Login'
+import Navbar from './Components/Navbar'
+import Hero from './Pages/Home'
 
 function App() {
   return (
     <>
-      <CandidateList />
-      <Login />
+      <Navbar />
+      <Hero/>
     </>
   )
 }
