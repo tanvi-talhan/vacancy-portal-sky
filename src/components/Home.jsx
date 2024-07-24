0ea453c2a79881feb49f2532786f594c53b38025
Piyush Mahendra Falke <72334439+codemonic-zero@users.noreply.github.com>
2024-07-24 13:11:37 +0530

    "creative changes"

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,23 +5,26 @@ import logo3 from '../assets/Home/c3.jpg';
 import logo4 from '../assets/Home/c4.jpg';
 import logo5 from '../assets/Home/c5.jpeg';
 import logo6 from '../assets/Home/c6.jpg';
+import '../index.css'; // Ensure this path is correct
+
 
 // SearchBar Component
 const SearchBar = ({ value, onChange }) => {
   return (
-    <div className="flex justify-center mt-4">
-      <div className="flex items-center bg-green-950 border rounded-md p-1 w-60">
-        <span className="text-green-500 mr-2 text-xl">&#x1F50D;</span> {/* Adjusted size */}
-        <span className="inline-flex items-center rounded-md bg-blue-50 px-2 py-1 text-xs font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10">
-        Vacancies
-</span>
-        <input
-          type="text"
-          placeholder=""
-          value={value}
-          onChange={onChange}
-          className="p-1 outline-none text-sm w-full bg-transparent text-white" // Adjusted padding and font size
-        />
+    <div className="relative mt-4">
+      <div className="flex justify-center">
+        <div className="max-w-9xl w-full mx-6 sm:mx-6 md:mx-16 lg:mx-80">
+          <div className="flex items-center bg-green-50 border rounded-full p-2">
+            <input
+              type="text"
+              placeholder="search for vacancies..."
+              value={value}
+              onChange={onChange}
+              className="p-1 outline-none text-sm w-full bg-transparent text-white"
+            />
+            <span className="relative right-2 top-3 transform -translate-y-1/2 text-green-500 text-xl">&#x1F50D;</span>
+          </div>
+        </div>
       </div>
     </div>
   );
@@ -51,7 +54,7 @@ const Home = () => {
         <img
           alt="Card 1"
           src={logo1}
-          className="h-48 w-auto rounded-lg"
+          className="h-96 w-auto rounded-lg"
         />
       ),
       title: 'Noteworthy Technology Acquisitions 2021',
@@ -62,7 +65,7 @@ const Home = () => {
         <img
           alt="Card 2"
           src={logo2}
-          className="h-48 w-auto rounded-lg"
+          className="h-96 w-auto rounded-lg"
         />
       ),
       title: 'Card 2 Title',
@@ -73,7 +76,7 @@ const Home = () => {
         <img
           alt="Card 3"
           src={logo3}
-          className="h-48 w-auto rounded-lg"
+          className="h-96 w-auto rounded-lg"
         />
       ),
       title: 'Card 3 Title',
@@ -84,7 +87,7 @@ const Home = () => {
         <img
           alt="Card 4"
           src={logo4}
-          className="h-48 w-auto rounded-lg"
+          className="h-96 w-auto rounded-lg"
         />
       ),
       title: 'Card 4 Title',
@@ -95,7 +98,7 @@ const Home = () => {
         <img
           alt="Card 5"
           src={logo5}
-          className="h-48 w-auto rounded-lg"
+          className="h-96 w-auto rounded-lg"
         />
       ),
       title: 'Card 5 Title',
@@ -106,7 +109,7 @@ const Home = () => {
         <img
           alt="Card 6"
           src={logo6}
-          className="h-48 w-auto rounded-lg"
+          className="h-96 w-auto rounded-lg"
         />
       ),
       title: 'Card 6 Title',
@@ -120,14 +123,22 @@ const Home = () => {
   );
 
   return (
+    <>    
     <div className="container mx-auto py-12">
+
       <SearchBar value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
-      <div className="grid grid-cols-1 gap-2 mt-8">
+
+        <div className="grid grid-cols-1 mt-8">
+          <span className="sticky top-0 ml-4 sm:ml-8 md:ml-24 lg:ml-60 xl:ml-96 bg-blue-200 px-2 py-1 text-lg font-medium text-blue-700 rounded-full text-gradient-my text-shadow-custom animate-pulse">
+            new vacancies
+          </span>
         {filteredCards.map((card, index) => (
           <Card key={index} img={card.img} title={card.title} description={card.description} />
         ))}
       </div>
     </div>
+    </>
+
   );
 };
 
