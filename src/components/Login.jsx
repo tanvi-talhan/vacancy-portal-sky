57a470f2fffaa471b58df19515975a7a537260d7
Radshu21 <tanvitalhan@gmail.com>
2024-08-06 16:09:31 +0530

    Changes in login and Sign up

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,6 +4,7 @@ import logo from "../assets/common/logo.png";
 import { Link, useNavigate } from 'react-router-dom';
 import { auth } from '../../firebase/firebaseConfig';  
 import { signInWithEmailAndPassword } from "firebase/auth";
+import Bgimage from '../assets/common/bg-img.png';
 
 export default function Login() {
     const [showPassword, setShowPassword] = useState(false);
@@ -33,14 +34,14 @@ export default function Login() {
 
     return (
         <>
-            <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-                <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-                    <img
-                        alt="Your Company"
-                        src={logo}
-                        className="mx-auto h-10 w-auto"
-                    />
-                    <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-green-950">
+      <div className="container py-12 bg-cover bg-no-repeat bg-center max-w-full min-h-screen flex flex-col items-center" style={{ backgroundImage: `url(${Bgimage})` }}>
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+            <img
+                alt="Your Company"
+                src={logo}
+                className="mx-auto h-10 w-auto"
+            />
+                    <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-yellow-200">
                         Log in to your account
                     </h2>
                 </div>
@@ -48,7 +49,7 @@ export default function Login() {
                 <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
                     <form onSubmit={handleSubmit} className="space-y-6">
                         <div>
-                            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                            <label htmlFor="email" className="block text-md font-medium leading-6 text-white">
                                 Email address
                             </label>
                             <div className="mt-2">
@@ -59,17 +60,17 @@ export default function Login() {
                                     required
                                     autoComplete="email"
                                     onChange={handleChange}
-                                    className="block w-full rounded-md border-green-500 border-2 p-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-600 sm:text-sm sm:leading-6"
+                                    className="block w-full rounded-md border-[#25433] border-2 p-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6"
                                 />
                             </div>
                         </div>
                         <div>
                             <div className="flex items-center justify-between">
-                                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                                <label htmlFor="password" className="block text-md font-medium leading-6 text-white">
                                     Password
                                 </label>
                                 <div className="text-sm">
-                                    <a href="#" className="font-semibold text-green-600 hover:text-green-500">
+                                    <a href="#" className="font-semibold text-yellow-200 hover:text-green-200">
                                         Forgot password?
                                     </a>
                                 </div>
@@ -82,7 +83,7 @@ export default function Login() {
                                     required
                                     autoComplete="current-password"
                                     onChange={handleChange}
-                                    className="block w-full rounded-md border-2 border-green-500 p-2 py-1.5 text-gray-900 shadow-sm placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-600 sm:text-sm sm:leading-6"
+                                    className="block w-full rounded-md border-2 border-[#254336] p-2 py-1.5 text-gray-900 shadow-sm placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6"
                                 />
                                 <button
                                     type="button"
@@ -101,16 +102,16 @@ export default function Login() {
                         <div>
                             <button
                                 type="submit"
-                                className="flex w-full justify-center rounded-md bg-green-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-green-50 hover:text-green-600 hover:border hover:border-green-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                className="flex w-full justify-center rounded-md bg-[#254336] px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-green-200 hover:text-green-800 hover:border hover:border-green-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
                             >
                                 Log in
                             </button>
                         </div>
                     </form>
 
-                    <p className="mt-10 text-center text-sm text-gray-500">
+                    <p className="mt-10 text-center text-sm text-gray-100">
                         Not Registered?&nbsp;
-                        <Link to='/register' className="font-semibold leading-6 text-green-600 hover:text-green-500">
+                        <Link to='/register' className="font-semibold leading-6 text-yellow-200 hover:text-green-200">
                             Create a new account!
                         </Link>
                     </p>
